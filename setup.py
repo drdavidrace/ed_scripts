f116a7c0b8dee0f36eb254666d722df58146697c
David Race <dr.david.race@gmail.com>
2018-12-15 19:59:22 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from distutils.core import setup
-import sys
-
+'''
+Setup for ed_scripts
+'''
 from setuptools import setup
 
 
-setup(name='in_array',
+setup(name='ed_scripts',
       version='0.0.1.a',
       author='Dr David Race',
       author_email='dr.david.race@gmail.com',
-      url='',
-      download_url='',
+      url='git+https://github.com/drdavidrace/ed_scripts.git',
       description='routines to convert Colaboratory forms input strings to arrays of floats or ints',
-      long_description='',
-      provides=['in_array.array_floats,in_array.array_ints'],
-      keywords='',
+      long_description=' ',
       license='GNU General Public License',
       packages=['ed_scripts'],
+      zip_safe=False
      )
