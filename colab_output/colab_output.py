61cf165594918e8822b2329f9ee416ed66a597c7
drdavidrace <41238989+drdavidrace@users.noreply.github.com>
2020-02-10 18:22:06 -0600

    fix HTML

--- colab_output/colab_output.py
+++ colab_output/colab_output.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from IPython.display import Math, Latex
+from IPython.display import HTML, Math, Latex
 import pkg_resources
 __version__ = pkg_resources.require('ed_scripts')[0].version
 def display_matrix(left_side = "A = ", input_sympy = None) -> None:
