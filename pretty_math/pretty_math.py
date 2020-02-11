8ce3a9155aeedf2cb641b032d78fc1fe0929ed0f
David Race <dr.david.race@gmail.com>
2020-02-11 12:12:12 -0600

    try again

--- pretty_math/pretty_math.py
+++ pretty_math/pretty_math.py
@@ -18,7 +18,7 @@ def display_sympy(left_side = "A = ", input_sympy = None) -> None:
         [type] -- [description]
     """
     assert input_sympy is not None
-    print((type(input_sympy).__name__).split())
+    print(isinstance(input_sympy,'sympy'))
     display(HTML("<script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/latest.js?config=default'></script>"))
     enhance_left = left_side.replace(" ","\\,")
     latex_sentence = "{}{}".format(enhance_left, sp.latex(input_sympy,mode='plain'))
