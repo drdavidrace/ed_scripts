a1d17a824c291c48ea4e6fd982061fdd135feaaa
David Race <dr.david.race@gmail.com>
2020-02-11 13:06:58 -0600

    again

--- build/lib/pretty_math/pretty_math.py
+++ build/lib/pretty_math/pretty_math.py
@@ -19,7 +19,7 @@ def display_sympy(left_side = "A = ", input_sympy = None) -> None:
     """
     assert input_sympy is not None
     print(input_sympy.__class__)
-    print(isinstance(input_sympy.__class__,<class 'sympy.matrices.dense.MutableDenseMatrix'>))
+    print(isinstance(input_sympy,tuple(sympy.core.all_classes)))
     display(HTML("<script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/latest.js?config=default'></script>"))
     enhance_left = left_side.replace(" ","\\,")
     latex_sentence = "{}{}".format(enhance_left, sp.latex(input_sympy,mode='plain'))
