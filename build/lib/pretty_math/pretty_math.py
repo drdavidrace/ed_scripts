3cfd7aea012d673c3dd9cb101ab3ef1e6a358f4b
David Race <dr.david.race@gmail.com>
2020-02-15 11:00:06 -0600

    generalize for display

--- build/lib/pretty_math/pretty_math.py
+++ build/lib/pretty_math/pretty_math.py
@@ -80,6 +80,37 @@ def display_j(in_list: list = None) -> int:
         return status
     return status
 #
+def display_d(in_list: list = None) -> int:
+    """This is a top level routine which displays output in Jupyter using mathjax
+        NOTE:  This is currently only tested with Google Colaboratory
+    
+    Keyword Arguments:
+        left_side {list} -- The list of stuff to display (default: {None})
+    
+    Returns:
+        int -- discription of the status
+    """
+    status = 0
+    try:
+        assert in_list is not None
+    except:
+        status = 1
+        print("The in_list must not be None.")
+        return status
+    #Main body of work
+    try:
+        display(HTML(_mathjax_sentence_))
+        #Obtain the latex
+        status, full_sentence = _display_l_(in_list,_pdf_math_eq_delim_)
+        #Use display(Math) to output the latex of the sympy expression to the output of a compute cell
+        display(Math(full_sentence))
+        return status
+    except Exception as e:
+        status = 2
+        print("Something went amiss with the mathjax process.  Details: {}".format(e))
+        return status
+    return status
+#
 #  Private routines
 #
 def _display_l_(in_list: list = None,eq_delim: str = _jup_math_eq_delim_) -> (int, str):
@@ -127,94 +158,94 @@ def _display_l_(in_list: list = None,eq_delim: str = _jup_math_eq_delim_) -> (in
         display_message = _begin_mult_sentence_ + message + _end_mult_sentence_
         return status, display_message
 #
-def _display_l_d_(in_list: list = None) -> (int, str):
-    """Create the output for latex version of output that is suitable for output that will be output to jupyter using mathjax
+# def _display_l_d_(in_list: list = None) -> (int, str):
+#     """Create the output for latex version of output that is suitable for output that will be output to jupyter using mathjax
 
-       Caveat:  It is assumed that the printing of the output to the file
-       is handled separately from this function.
+#        Caveat:  It is assumed that the printing of the output to the file
+#        is handled separately from this function.
     
-    Keyword Arguments:
-        in_list {list} -- The list of items to display (default: {None})
-        f {file-like object}  --  If f is None, then the result is returned to the caller
-            If f is not None, then the result is written to f
+#     Keyword Arguments:
+#         in_list {list} -- The list of items to display (default: {None})
+#         f {file-like object}  --  If f is None, then the result is returned to the caller
+#             If f is not None, then the result is written to f
     
-    Returns:
-        int -- the status of the processing
-        str -- the latex string to output that is suitable for multiline output
+#     Returns:
+#         int -- the status of the processing
+#         str -- the latex string to output that is suitable for multiline output
 
-    Assumptions:
-        Currently only strings, numbers, numpy.arrays and sympy expressions are allowed as inputs in the list.
+#     Assumptions:
+#         Currently only strings, numbers, numpy.arrays and sympy expressions are allowed as inputs in the list.
 
-        These are converted to sympy expressions for the last three and output using latex math
-    """
-    status = 0
-    output = None
-    try:
-        assert in_list is not None
-    except:
-        status = 1
-        output = "The input must not be None."
-        return status, output
-    work = []
-    try:
-        assert isinstance(in_list, list)
-        work = in_list
-    except:
-        work = [in_list]
-    #  If we reach this point, we should have a list
-    status, message = _create_latex_sentence_d_(work)
-    #Write the output
-    if (status > 0) or (len(message) == 0):
-        status = 2
-        return status, message
-    else:
-        display_message = _begin_mult_sentence_ + message + _end_mult_sentence_
-        return status, display_message
+#         These are converted to sympy expressions for the last three and output using latex math
+#     """
+#     status = 0
+#     output = None
+#     try:
+#         assert in_list is not None
+#     except:
+#         status = 1
+#         output = "The input must not be None."
+#         return status, output
+#     work = []
+#     try:
+#         assert isinstance(in_list, list)
+#         work = in_list
+#     except:
+#         work = [in_list]
+#     #  If we reach this point, we should have a list
+#     status, message = _create_latex_sentence_d_(work)
+#     #Write the output
+#     if (status > 0) or (len(message) == 0):
+#         status = 2
+#         return status, message
+#     else:
+#         display_message = _begin_mult_sentence_ + message + _end_mult_sentence_
+#         return status, display_message
 #
-def _display_l_p_(in_list: list = None) -> (int, str):
-    """Create the output for latex version of output that is suitable for output that will be turned into
-        pdf
+# def _display_l_p_(in_list: list = None) -> (int, str):
+#     """Create the output for latex version of output that is suitable for output that will be turned into
+#         pdf
 
-       Caveat:  It is assumed that the printing of the output to the file
-       is handled separately from this function.
+#        Caveat:  It is assumed that the printing of the output to the file
+#        is handled separately from this function.
     
-    Keyword Arguments:
-        in_list {list} -- The list of items to display (default: {None})
-        f {file-like object}  --  If f is None, then the result is returned to the caller
-            If f is not None, then the result is written to f
+#     Keyword Arguments:
+#         in_list {list} -- The list of items to display (default: {None})
+#         f {file-like object}  --  If f is None, then the result is returned to the caller
+#             If f is not None, then the result is written to f
     
-    Returns:
-        int -- the status of the processing
-        str -- the latex string to output that is suitable for multiline output
+#     Returns:
+#         int -- the status of the processing
+#         str -- the latex string to output that is suitable for multiline output
 
-    Assumptions:
-        Currently only strings, numbers, numpy.arrays and sympy expressions are allowed as inputs in the list.
+#     Assumptions:
+#         Currently only strings, numbers, numpy.arrays and sympy expressions are allowed as inputs in the list.
 
-        These are converted to sympy expressions for the last three and output using latex math
-    """
-    status = 0
-    output = None
-    try:
-        assert in_list is not None
-    except:
-        status = 1
-        output = "The input must not be None."
-        return status, output
-    work = []
-    try:
-        assert isinstance(in_list, list)
-        work = in_list
-    except:
-        work = [in_list]
-    #  If we reach this point, we should have a list
-    status, message = _create_latex_sentence_p_(work)
-    #Write the output
-    if (status > 0) or (len(message) == 0):
-        status = 2
-        return status, message
-    else:
-        display_message = _begin_mult_sentence_ + message + _end_mult_sentence_
-        return status, display_message
+#         These are converted to sympy expressions for the last three and output using latex math
+#     """
+#     status = 0
+#     output = None
+#     try:
+#         assert in_list is not None
+#     except:
+#         status = 1
+#         output = "The input must not be None."
+#         return status, output
+#     work = []
+#     try:
+#         assert isinstance(in_list, list)
+#         work = in_list
+#     except:
+#         work = [in_list]
+#     #  If we reach this point, we should have a list
+#     status, message = _create_latex_sentence_p_(work)
+#     #Write the output
+#     if (status > 0) or (len(message) == 0):
+#         status = 2
+#         return status, message
+#     else:
+#         display_message = _begin_mult_sentence_ + message + _end_mult_sentence_
+#         return status, display_message
 #
 def _create_latex_sentence_(input_val: list = None, eq_delim: str = _jup_math_eq_delim_) -> (int, str):
     """
@@ -265,103 +296,103 @@ def _create_latex_sentence_(input_val: list = None, eq_delim: str = _jup_math_eq
     message = out_str
     return status, message
 #
-def _create_latex_sentence_p_(input_val: list = None) -> (int, str):
-    """
-    Internal routine to create a latex sentence from components of a list or tuple
+# def _create_latex_sentence_p_(input_val: list = None) -> (int, str):
+#     """
+#     Internal routine to create a latex sentence from components of a list or tuple
     
-    Keyword Arguments:
-        input_array {list, tuple} -- a list or tuple of elements to create a latex sentence (default: {None})
+#     Keyword Arguments:
+#         input_array {list, tuple} -- a list or tuple of elements to create a latex sentence (default: {None})
     
-    Returns:
-        int - status of the processing
-        str - the latex output of the list in the form of a string
-    """
-    work = []
-    try:
-        if isinstance(input_val, list):
-            work = input_val
-        elif isinstance(input_val, str):
-            work = [input_val]
-        elif isinstance(input_val,numbers.Number):
-            work = [input_val]
-        elif isinstance(input_val,sp.Basic):
-            work = [input_val]
-        else:
-            work = list(input_val)
-    except:
-        status = 1
-        message = "The input must be able to be turned into a list"
-        return status, message
-    status = 0
-    out_str = ""
-    for v in work:
-        if isinstance(v,str):
-            out_str += (" " + v)
-        elif isinstance(v,numbers.Number):  #This works for numpy numbers also
-            out_str += (" {}".format(v))
-        elif isinstance(v, np_arrays):
-            x = sp.symbols('x')
-            x = sp.Matrix(v)
-            out_str += (" $" + sp.latex(x,mode='plain') + "$")
-        else:
-            try:
-                assert v.has(sp.Basic)
-                out_str += (" $" + sp.latex(v,mode='plain') + "$")
-            except:
-                status = 2
-                message = "The inputs must be a str, number, np number, np.array or sympy expression: {}".format(v)
-                return status, message
-    message = out_str
-    return status, message
-#
-def _create_latex_sentence_d_(input_val: list = None) -> (int, str):
-    """
-    Internal routine to create a latex sentence from components of a list or tuple suitable for display in 
+#     Returns:
+#         int - status of the processing
+#         str - the latex output of the list in the form of a string
+#     """
+#     work = []
+#     try:
+#         if isinstance(input_val, list):
+#             work = input_val
+#         elif isinstance(input_val, str):
+#             work = [input_val]
+#         elif isinstance(input_val,numbers.Number):
+#             work = [input_val]
+#         elif isinstance(input_val,sp.Basic):
+#             work = [input_val]
+#         else:
+#             work = list(input_val)
+#     except:
+#         status = 1
+#         message = "The input must be able to be turned into a list"
+#         return status, message
+#     status = 0
+#     out_str = ""
+#     for v in work:
+#         if isinstance(v,str):
+#             out_str += (" " + v)
+#         elif isinstance(v,numbers.Number):  #This works for numpy numbers also
+#             out_str += (" {}".format(v))
+#         elif isinstance(v, np_arrays):
+#             x = sp.symbols('x')
+#             x = sp.Matrix(v)
+#             out_str += (" $" + sp.latex(x,mode='plain') + "$")
+#         else:
+#             try:
+#                 assert v.has(sp.Basic)
+#                 out_str += (" $" + sp.latex(v,mode='plain') + "$")
+#             except:
+#                 status = 2
+#                 message = "The inputs must be a str, number, np number, np.array or sympy expression: {}".format(v)
+#                 return status, message
+#     message = out_str
+#     return status, message
+# #
+# def _create_latex_sentence_d_(input_val: list = None) -> (int, str):
+#     """
+#     Internal routine to create a latex sentence from components of a list or tuple suitable for display in 
     
-    Keyword Arguments:
-        input_array {list, tuple} -- a list or tuple of elements to create a latex sentence (default: {None})
+#     Keyword Arguments:
+#         input_array {list, tuple} -- a list or tuple of elements to create a latex sentence (default: {None})
     
-    Returns:
-        int - status of the processing
-        str - the latex output of the list in the form of a string
-    """
-    work = []
-    try:
-        if isinstance(input_val, list):
-            work = input_val
-        elif isinstance(input_val, str):
-            work = [input_val]
-        elif isinstance(input_val,numbers.Number):
-            work = [input_val]
-        elif isinstance(input_val,sp.Basic):
-            work = [input_val]
-        else:
-            work = list(input_val)
-    except:
-        status = 1
-        message = "The input must be able to be turned into a list"
-        return status, message
-    status = 0
-    out_str = ""
-    for v in work:
-        if isinstance(v,str):
-            out_str += (" " + v)
-        elif isinstance(v,numbers.Number):  #This works for numpy numbers also
-            out_str += (" {}".format(v))
-        elif isinstance(v, np_arrays):
-            x = sp.symbols('x')
-            x = sp.Matrix(v)
-            out_str += (" " + sp.latex(x,mode='plain') + "")
-        else:
-            try:
-                assert v.has(sp.Basic)
-                out_str += (" " + sp.latex(v,mode='plain') + "")
-            except:
-                status = 2
-                message = "The inputs must be a str, number, np number, np.array or sympy expression: {}".format(v)
-                return status, message
-    message = out_str
-    return status, message
+#     Returns:
+#         int - status of the processing
+#         str - the latex output of the list in the form of a string
+#     """
+#     work = []
+#     try:
+#         if isinstance(input_val, list):
+#             work = input_val
+#         elif isinstance(input_val, str):
+#             work = [input_val]
+#         elif isinstance(input_val,numbers.Number):
+#             work = [input_val]
+#         elif isinstance(input_val,sp.Basic):
+#             work = [input_val]
+#         else:
+#             work = list(input_val)
+#     except:
+#         status = 1
+#         message = "The input must be able to be turned into a list"
+#         return status, message
+#     status = 0
+#     out_str = ""
+#     for v in work:
+#         if isinstance(v,str):
+#             out_str += (" " + v)
+#         elif isinstance(v,numbers.Number):  #This works for numpy numbers also
+#             out_str += (" {}".format(v))
+#         elif isinstance(v, np_arrays):
+#             x = sp.symbols('x')
+#             x = sp.Matrix(v)
+#             out_str += (" " + sp.latex(x,mode='plain') + "")
+#         else:
+#             try:
+#                 assert v.has(sp.Basic)
+#                 out_str += (" " + sp.latex(v,mode='plain') + "")
+#             except:
+#                 status = 2
+#                 message = "The inputs must be a str, number, np number, np.array or sympy expression: {}".format(v)
+#                 return status, message
+#     message = out_str
+#     return status, message
 #
 def _get_latex_sympy_(left_side = None, input_sympy = None) -> str:
     """Internal routine to get the latex for for a sympy element using MathJax
