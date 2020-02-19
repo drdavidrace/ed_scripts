a53f13d85e7c8d82bb5e5836bb7596ee8881ea2c
David Race <dr.david.race@gmail.com>
2020-02-19 13:13:12 -0600

    again

--- diffe_math/diffe_math.py
+++ diffe_math/diffe_math.py
@@ -42,8 +42,10 @@ def computeN(start=None,end=None,h=None):
     Output:
         N - The number of intervals
     '''
+    print(np.fabs((float(end) - float(start))))
+    print((np.fabs((float(end) - float(start))/float(h))))
     try:
-        return int(np.ceil(fabs((float(end) - float(start))/float(h))))
+        return int(np.ceil(np.fabs((float(end) - float(start))/float(h))))
     except:
         return None
 #
