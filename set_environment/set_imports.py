f976027b9c3e748fc14999f0bdb275afe77bc270
Dr David Race <dr.david.race@gmail.com>
2020-05-06 10:54:24 -0500

    adding set_up_gpu

--- set_environment/set_imports.py
+++ set_environment/set_imports.py
@@ -19,11 +19,11 @@ def show_environment():
     #  Checks
     print("Python Major Version: {}".format(sys.version_info.major))
     print("Python Minor Version: {}".format(sys.version_info.minor))
-    if (py_version.major < 3):
+    if (sys.version_info.major < 3):
         print("This code required Python 3")
-    elif (py_version.major > 3):
+    elif (sys.version_info.major > 3):
         print("This code was built with Python 3, but this may work since the major version is higher.")
-    elif (py_version.minor < 6):
+    elif (sys.version_info.minor < 6):
         print("This code was built with Python 3.6, but this may work.  You might want to upgrade to version 3.6 or higher.")
     else:
         print("This code should work.")
@@ -36,3 +36,29 @@ def show_environment():
     print("This code uses Matplotlib version:{}".format(mpl.__version__))
     print("This code uses PyTorch version:  {}".format(T.__version__))
     print("This notebook uses in_array version: {}".format(in_array.__version__))
+#
+def set_up_gpu():
+    global use_double, device, float_dtype, int_dtype
+    use_double = None
+    device = None
+    float_dtype = None
+    int_dtype = None
+    if T.cuda_is_available():
+        device = 'cuda' if T.cuda.is_available() else 'cpu'
+        float_dtype = T.double if use_double else T.float
+        int_dtype = T.long if use_double else T.int
+        #Basic PyTorch Information
+
+        print("The PyTorch device: {}".format(device))
+        print("The PyTroch float dtype: {}".format(float_dtype))
+        print("The PyTorch int dtype: {}".format(int_dtype))
+        #Set the default types
+        if T.cuda.is_available():
+            T.cuda.device(device=device)
+        T.set_default_dtype(float_dtype)
+        print("Verifying the default dtype is 64 bit: {}".format(T.get_default_dtype()))
+        print("Verifying the default device: {}".format(T.cuda.current_device()))
+        return True
+    else:
+        return False
+
