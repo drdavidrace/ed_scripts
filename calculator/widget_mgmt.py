d1904721fae53ef859f6ea52584850618c14e5ec
David Race <dr.david.race@gmail.com>
2020-06-01 15:36:44 -0500

    minor error

--- calculator/widget_mgmt.py
+++ calculator/widget_mgmt.py
@@ -93,7 +93,7 @@ def build_matrix_input(num_row:int = None, num_col:int = None) ->  List:
             children.append(inputs[i][j])
         pos_names = pos_names + row_pos_names + "\n"
     #Build the Display
-    grid_template_rows = ["auto " for i in range(num_row + 1)]
+    grid_template_rows = "".join(["auto " for i in range(num_row + 1)])
     width_column = int(100//num_col)
     width_first_col = 100 - width_column * num_col
     grid_template_columns = "{}% ".format(width_first_col) + "".join(["{}%".format(width_column) for i in range(num_col)])
