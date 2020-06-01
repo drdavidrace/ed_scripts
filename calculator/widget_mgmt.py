23b1c9aaee5671640634b7661f97eba599dfcc17
David Race <dr.david.race@gmail.com>
2020-06-01 15:28:52 -0500

    start inputing matrix

--- calculator/widget_mgmt.py
+++ calculator/widget_mgmt.py
@@ -21,7 +21,7 @@ def set_default_numeric_values(num_variables:int = 5) ->  Tuple:
     default values are 0 to num_variables - 1.
     """
     assert isinstance(num_variables, int)
-    return (sp.Integer(i) for i in range(num_variables))
+    return (i for i in range(num_variables))
 #
 def set_default_matrix_values(num_variables:int = 5) ->  Tuple:
     """Returns a default list of variables to help keep the code clean
@@ -48,3 +48,64 @@ def get_default_var_names(num_variables:int = 5, start_char:str = "a") -> List:
         print("{} must be a lower case or upper case character".format(start_char))
         assert False
     return [str(chr(i)) for i in range(ord(start_char), ord(start_char) + num_variables)]
+#
+def build_matrix_input(num_row:int = None, num_col:int = None) ->  List:
+    assert num_row is not None
+    assert isinstance(num_row, int)
+    assert num_row >= 1 and num_row <= 10
+    assert num_col is not None
+    assert isinstance(num_col, int)
+    assert num_col >= 1 and num_col <= 10
+    inputs = []
+    for i in range(num_row):
+        row_list = [None for j in range(num_col)]
+        inputs.append(row_list)
+    pos_names = ""
+    children = []
+    #Build the inputs
+    for i in range(num_row + 1):
+        row_pos_names = ""
+        for j in range(num_col+1):
+ 
+            pos_i_j = None
+            if i == 0:
+                if j == 0:
+                    pos_i_j = "pos_{}_{}".format(i,j)
+                    inputs[i][j] = Output(layout=Layout(width='auto',grid_area = pos_i_j))
+                    with inputs[i][j]:
+                        print("")
+                else:
+                    pos_i_j = "pos_{}_{}".format(i,j)
+                    inputs[i][j] = Output(layout=Layout(width='auto',grid_area = pos_i_j))
+                    with inputs[i][j]:
+                        print("       {}       ".format(j))
+                row_pos_names = row_pos_names + pos_i_j + " "
+            else:
+                if j == 0:
+                    pos_i_j = "pos_{}_{}".format(i,j)
+                    inputs[i][j] = Output(layout=Layout(width='auto',grid_area=pos_i_j))
+                    with inputs[i][j]:
+                        print(" {} ".format(i))
+                else:
+                    pos_i_j = "pos_{}_{}".format(i,j)
+                    inputs[i][j] = FloatText(value=0.,disabled=False,layout=Layout(width='auto',grid_area=pos_i_j))
+                row_pos_names = row_pos_names + pos_i_j + " "
+            children.append(inputs[i][j])
+        pos_names = pos_names + row_pos_names + "\n"
+    #Build the Display
+    grid_template_rows = ["auto " for i in range(num_row + 1)]
+    width_column = int(100//num_col)
+    width_first_col = 100 - width_column * num_col
+    grid_template_columns = "{}% ".format(width_first_col) + ["{}%".format(width_column) for i in range(num_col)]
+    #display the inputs
+    matrix_grid = GridBox(children=children,
+        layout = Layout(
+            width="75%",
+            grid_template_rows = grid_template_rows,
+            grid_template_columns = grid_template_columns,
+            grid_template_areas = pos_names
+            )
+        )
+    display(matrix_grid)
+    #
+    return inputs
